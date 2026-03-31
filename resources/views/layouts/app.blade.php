ce313f4135b73d4c3b0ef44ac60b5c33116f52cb
mingxuanyap <mingxuanyap622@gmail.com>
2026-03-31 16:02:21 +0800

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,6 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
-    <meta name="theme-color" content="#111111">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <meta name="apple-mobile-web-app-title" content="Extech Studio">
-    <link rel="apple-touch-icon" href="{{ asset('icons/icon-192.png') }}">
 </head>
 
 <body class="font-sans antialiased bg-black text-gray-200">
